f16693b53987606569e256c7d24c2d6573b24f57
Rahul Pant <rahulpant2002@gmail.com>
2025-02-17 16:07:35 +0530

    Color Updated

--- src/components/BackgroundImg.jsx
+++ src/components/BackgroundImg.jsx
@@ -20,7 +20,7 @@ const BackgroundImg = ({img, heading, text, scrollTo}) => {
             <p className="mt-4 bg-gradient-to-r from-white to-yellow-500 text-transparent bg-clip-text">
               {text}
             </p>
-            <button onClick={handleBtn} className="mt-4 px-6 py-2 cursor-pointer bg-yellow-500 text-black font-semibold rounded-lg transition-all duration-300 hover:scale-110">
+            <button onClick={handleBtn} className="mt-4 px-6 py-2 cursor-pointer bg-balck text-white font-semibold rounded-lg transition-all duration-300 hover:scale-110">
             Know More
             </button>
         </div>
