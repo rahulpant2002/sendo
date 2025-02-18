90d0392d737259f71436ab605c135ddc8e629742
Rahul Pant <rahulpant2002@gmail.com>
2025-02-18 19:28:41 +0530

    Meta Data Updated

--- src/components/HomePage/TestimonialSlider.jsx
+++ src/components/HomePage/TestimonialSlider.jsx
@@ -22,7 +22,7 @@ const testimonials = [
 
 const TestimonialSlider = () => {
   return (
-    <div className="max-w-3xl mx-auto p-6 text-white bg-black rounded-md my-5">
+    <div className="max-w-3xl mx-auto p-6 text-white bg-black rounded-md my-5 px-5">
       <h2 className="text-center text-2xl font-bold mb-6">Testimonials</h2>
       <Swiper
         pagination={{
