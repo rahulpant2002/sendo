3ab00e7710a0604330c62ca7e69fb2ae45308e81
Rahul Pant <rahulpant2002@gmail.com>
2025-02-19 14:19:11 +0530

    Refresh Issue resolving

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    historyApiFallback: true, // Ensures correct routing on refresh
+  },
   // resolve: {
   //   alias: {
   //     '@': path.resolve(__dirname, 'src'),
